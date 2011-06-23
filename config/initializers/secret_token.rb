47dc1a46374c1397780b20741296924dcc8b9fc2
Dev Server <dev@dev.com>
2011-06-23 20:13:18 +0000

    created ruby repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ror::Application.config.secret_token = '4550d6e88aeffbdbc836d58d1c149cdb38f710b5f82b30fe76a9cc04202f8dda62f0f19970d64116001f815a8c613b3f6e27d31225eb079eb3fdf818e3cce770'
